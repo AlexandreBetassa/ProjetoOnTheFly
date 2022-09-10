9208dea7f40f0869c17b497b61c90076b481fab9
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-09-09 21:03:52 -0300

    Criado lista de iatas e recuperação de arquivos

--- Project_OnTheFly/Program.cs
+++ Project_OnTheFly/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Project_OnTheFly
 {
@@ -6,7 +8,42 @@ namespace Project_OnTheFly
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<String> listaIatas = new List<string>();
+            LerArquivo(listaIatas);
+            Console.WriteLine("Imprimir lista de iatas ");
+            foreach (var item in listaIatas) Console.WriteLine(item);
+            Console.ReadKey();
+
+
+        }
+
+        static void LerArquivo(List<String> lista)
+        {
+            string line;
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\Alexandre\\Desktop\\Aulas\\Aeroporto\\Project_OnTheFly\\Project_OnTheFly\\listaIatas.txt");//Instancia um Objeto StreamReader (Classe de Manipulação de Leitura de Arquivos)
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    line = sr.ReadLine();
+                    lista.Add(line);
+                }
+                sr.Close();
+                Console.WriteLine("Fim da Leitura do Arquivo");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executando o Bloco de Comando - Sem Erros");
+            }
+            Console.ReadKey();
+            Console.Clear();
+            return;
         }
     }
 }
