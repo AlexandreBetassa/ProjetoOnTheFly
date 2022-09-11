a769041ba1c9600964b8b53ed73b2a0c2129bff2
Thalya Teixeira <thalyateixeira9@hotmail.com>
2022-09-11 17:02:07 -0300

    Inserindo classe de aeronave

---
+++ Project_OnTheFly/Voo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_OnTheFly
+{
+    internal class Voo
+    {
+        public string IdVoo { get; set; }
+        public string Destino { get; set; }
+        //IDAeronave
+        public DateTime DataVoo { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public char Situacao { get; set; }
+
+        public Voo()
+        {
+
+        }
+
+        public Voo(string idVoo, string destino, DateTime dataVoo, DateTime dataCadastro, char situacao)
+        {
+            IdVoo = idVoo;
+            Destino = destino;
+            //IdAeronave
+            DataVoo = dataVoo;
+            DataCadastro = dataCadastro;
+            Situacao = situacao;
+        }
+
+        public void CadastrarVoo()
+        {
+
+        }
+    }
+}
