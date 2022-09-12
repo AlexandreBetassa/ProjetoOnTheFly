986ad49f2bc05cf6c37a6f83d24e462a6a60676c
FelipeCaiko <felipecaiko@gmail.com>
2022-09-12 08:49:35 -0300

    Construção método ItemVenda

--- Project_OnTheFly/ItemVenda.cs
+++ Project_OnTheFly/ItemVenda.cs
@@ -8,5 +8,19 @@ namespace Project_OnTheFly
 {
     internal class ItemVenda
     {
+        public string IdItemVenda { get; set; }
+        public PassagemVoo PassagemVoo { get; set; }
+        public float ValorUnit { get; set; }
+
+        public void CadastrarItemVenda()
+        {
+            Console.WriteLine(">>>CADASTRO DE VENDA DE ITEM<<<");
+
+            //Inserir ID Venda
+
+            //Puxar ID da Passagem e Valor 
+
+        }
+
     }
 }
