d17ee5776eea2c2b582bed81a9008a61deca00fe
Thalya Teixeira <thalyateixeira9@hotmail.com>
2022-09-13 11:03:57 -0300

    correcoes no metodo editar passagem voo

--- Project_OnTheFly/PassagemVoo.cs
+++ Project_OnTheFly/PassagemVoo.cs
@@ -96,12 +96,12 @@ namespace Project_OnTheFly
                 Console.Write("0 - Sair");
                 op = int.Parse(Console.ReadLine());
 
-                 if (op != "1" && op != "2" && op != "0")
+                 if (op != 1 && op != 2 && op != 0)
                  {
                     Console.WriteLine("Opção inválida!");
                  }
 
-            } while (op != "1" && op != "2" && op != "0");
+            } while (op != 1 && op != 2 && op != 0);
 
                 switch (op)
                 {
